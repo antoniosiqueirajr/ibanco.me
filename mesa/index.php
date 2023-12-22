a0f9661ce6a19e6c71ef321d78d2686807e51f40
Antonio Siqueira <antoniofsiqueirajr@gmail.com>
2023-12-22 09:15:20 -0300

    Update index.php

--- mesa/index.php
+++ mesa/index.php
@@ -1811,7 +1811,7 @@
     <div class="footer">
       <div class="row justify-content-between align-items-center">
         <div class="col">
-          <p class="fs-6 mb-0">&copy; Front. <span class="d-none d-sm-inline-block">2022 Htmlstream.</span></p>
+          <p class="fs-6 mb-0">&copy; iBanco Me. <span class="d-none d-sm-inline-block">2024 iBanco.</span></p>
         </div>
         <!-- End Col -->
 
