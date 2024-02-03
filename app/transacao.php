e3ff74c56f6f74c28695d6645b0493191ab9085b
Antonio Siqueira <antoniofsiqueirajr@gmail.com>
2024-02-03 17:02:33 -0300

    Update transacao.php

--- app/transacao.php
+++ app/transacao.php
@@ -1,17 +1,6 @@
 ﻿
 <!DOCTYPE html>
-<!--
-Author: Keenthemes
-Product Name: Metronic 
-Product Version: 8.2.3
-Purchase: https://1.envato.market/EA4JP
-Website: http://www.keenthemes.com
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
+
 <html lang="en">
     <!--begin::Head-->
     <head>
@@ -111,7 +100,7 @@ License: For each use you must have a valid license purchased only from above li
 		<!--begin::Logo-->
 		<div class="aside-logo d-none d-lg-flex flex-column align-items-center flex-column-auto py-10" id="kt_aside_logo">		
 			<a href="/metronic8/demo7/../demo7/index.html">
-				<img alt="Logo" src="../../assets/media/logos/demo7.svg" class="h-35px">
+				<img alt="Logo" src="../assets/media/logos/demo7.svg" class="h-35px">
 			</a>
 		</div>
 		<!--end::Logo-->
