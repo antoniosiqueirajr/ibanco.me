7ac0644c35e19b047d9f7ccf8e48a2d0383b4fc5
Antonio Siqueira <antoniofsiqueirajr@gmail.com>
2024-02-04 12:04:09 -0300

    Update menu.php

--- app/menu.php
+++ app/menu.php
@@ -1912,26 +1912,6 @@
                                                 <!--end:Menu item-->
 
 
-                                                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-                                                    <!--begin:Menu link--><span class="menu-link"><span
-                                                            class="menu-icon"><i
-                                                                class="ki-duotone ki-element-plus fs-2"><span
-                                                                    class="path1"></span><span
-                                                                    class="path2"></span><span
-                                                                    class="path3"></span><span
-                                                                    class="path4"></span><span
-                                                                    class="path5"></span></i></span><span
-                                                            class="menu-title">Cliente</span><span
-                                                            class="menu-arrow"></span></span><!--end:Menu link--><!--begin:Menu sub-->
-                                                    <div class="menu-sub menu-sub-accordion"><!--begin:Menu item-->
-                                                        <div class="menu-item"><!--begin:Menu link-->
-                                                            <a class="menu-link" href=""><span class="menu-bullet"><span
-                                                                        class="bullet bullet-dot"></span></span><span
-                                                                    class="menu-title">Listagem</span>
-                                                            </a><!--end:Menu link-->
-                                                        </div><!--end:Menu item--><!--begin:Menu item-->
-                                                    </div><!--end:Menu sub-->
-                                                </div><!--end:Menu item--><!--begin:Menu item-->
                                                 <div class="menu-item">
                                                     <!--begin:Menu link--><a class="menu-link"
                                                         href="https://developer.ibanco.me" target="_blank"><span
@@ -1946,50 +1926,7 @@
                                                 </div>
                                                 <!--end:Menu item-->
 
-                                                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-                                                    <!--begin:Menu link--><span class="menu-link"><span
-                                                            class="menu-icon"><i
-                                                                class="ki-duotone ki-element-plus fs-2"><span
-                                                                    class="path1"></span><span
-                                                                    class="path2"></span><span
-                                                                    class="path3"></span><span
-                                                                    class="path4"></span><span
-                                                                    class="path5"></span></i></span><span
-                                                            class="menu-title">Contas Receber</span><span
-                                                            class="menu-arrow"></span></span><!--end:Menu link--><!--begin:Menu sub-->
-                                                    <div class="menu-sub menu-sub-accordion"><!--begin:Menu item-->
-                                                        <div class="menu-item"><!--begin:Menu link-->
-                                                            <a class="menu-link" href=""><span class="menu-bullet"><span
-                                                                        class="bullet bullet-dot"></span></span><span
-                                                                    class="menu-title">Listagem</span>
-                                                            </a><!--end:Menu link-->
-                                                        </div><!--end:Menu item--><!--begin:Menu item-->
-                                                    </div><!--end:Menu sub-->
-                                                </div><!--end:Menu item--><!--begin:Menu item-->
-
-                                                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-                                                    <!--begin:Menu link--><span class="menu-link"><span
-                                                            class="menu-icon"><i
-                                                                class="ki-duotone ki-element-plus fs-2"><span
-                                                                    class="path1"></span><span
-                                                                    class="path2"></span><span
-                                                                    class="path3"></span><span
-                                                                    class="path4"></span><span
-                                                                    class="path5"></span></i></span><span
-                                                            class="menu-title">Contas Pagar</span><span
-                                                            class="menu-arrow"></span></span><!--end:Menu link--><!--begin:Menu sub-->
-                                                    <div class="menu-sub menu-sub-accordion"><!--begin:Menu item-->
-                                                        <div class="menu-item"><!--begin:Menu link-->
-                                                            <a class="menu-link" href=""><span class="menu-bullet"><span
-                                                                        class="bullet bullet-dot"></span></span><span
-                                                                    class="menu-title">Listagem</span>
-                                                            </a><!--end:Menu link-->
-                                                        </div><!--end:Menu item--><!--begin:Menu item-->
-                                                    </div><!--end:Menu sub-->
-                                                </div><!--end:Menu item--><!--begin:Menu item-->
-
-
-
+                        
 
 
 
