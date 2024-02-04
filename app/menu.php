03d958a8e1e98b8b73484097ae35420466fd787e
Antonio Siqueira <antoniofsiqueirajr@gmail.com>
2024-02-04 12:38:45 -0300

    Update menu.php

--- app/menu.php
+++ app/menu.php
@@ -1496,7 +1496,7 @@
                                             <div id="kt_aside_menu_wrapper" class="menu-fit">
 
                                                 <!--begin:Menu item-->
-                                                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                                                <div data-kt-menu-trigger="click" class="menu-item here show menu-accordion">
                                                     <!--begin:Menu link--><span class="menu-link"><span class="menu-icon"><i class="ki-duotone ki-element-11 fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Dashboards</span><span class="menu-arrow"></span></span><!--end:Menu link--><!--begin:Menu sub-->
                                                     <div class="menu-sub menu-sub-accordion"><!--begin:Menu item-->
                                                         <div class="menu-item"><!--begin:Menu link--><a class="menu-link" href="../index.html"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">Default</span></a><!--end:Menu link-->
