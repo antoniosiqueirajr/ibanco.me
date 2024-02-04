16819aa664203fe7bf452045acc3f472672a1b10
Antonio Siqueira <antoniofsiqueirajr@gmail.com>
2024-02-04 15:38:24 -0300

    Update menu.php

--- app/menu.php
+++ app/menu.php
@@ -1793,6 +1793,64 @@
                                         </div>
                                         <!--end::Notifications-->
 
+                                                     <!--begin::Menu-->
+                                                     <div class="menu menu-column menu-fit menu-rounded menu-title-gray-600 menu-icon-gray-500 menu-state-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500 fw-semibold fs-5 px-6 my-5 my-lg-0" id="kt_aside_menu" data-kt-menu="true">
+
+<div id="kt_aside_menu_wrapper" class="menu-fit">
+    <!--begin:Menu item-->
+    <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+        <!--begin:Menu link--><span class="menu-link"><span class="menu-icon"><i class="ki-duotone ki-element-11 fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Dashboards</span><span class="menu-arrow"></span></span><!--end:Menu link--><!--begin:Menu sub-->
+        <div class="menu-sub menu-sub-accordion"><!--begin:Menu item-->
+            <div class="menu-item"><!--begin:Menu link--><a class="menu-link" href="../index.html"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">Default</span></a><!--end:Menu link-->
+            </div><!--end:Menu item--><!--begin:Menu item-->
+            <div class="menu-item"><!--begin:Menu link--><a class="menu-link active" href="ecommerce.html"><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">eCommerce</span></a><!--end:Menu link-->
+            </div><!--end:Menu item--><!--begin:Menu item-->
+
+
+
+        </div><!--end:Menu sub-->
+    </div><!--end:Menu item--><!--begin:Menu item-->
+    <div class="menu-item pt-5"><!--begin:Menu content-->
+        <div class="menu-content"><span class="menu-heading fw-bold text-uppercase fs-7">Intranet
+                iBanco</span>
+        </div><!--end:Menu content-->
+    </div><!--end:Menu item--><!--begin:Menu item-->
+
+    <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+        <!--begin:Menu link--><span class="menu-link"><span class="menu-icon"><i class="ki-duotone ki-element-plus fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i></span><span class="menu-title">Contas</span><span class="menu-arrow"></span></span><!--end:Menu link--><!--begin:Menu sub-->
+        <div class="menu-sub menu-sub-accordion"><!--begin:Menu item-->
+            <div class="menu-item"><!--begin:Menu link-->
+                <a class="menu-link" href=""><span class="menu-bullet"><span class="bullet bullet-dot"></span></span><span class="menu-title">Clientes</span>
+                </a><!--end:Menu link-->
+            </div><!--end:Menu item--><!--begin:Menu item-->
+        </div><!--end:Menu sub-->
+    </div><!--end:Menu item--><!--begin:Menu item-->
+
+
+    <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+        <!--begin:Menu link-->
+        <span class="menu-link"><span class="menu-icon"><i class="ki-duotone ki-element-plus fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i></span><span class="menu-title">Empréstimos</span>
+            <span class="menu-arrow"></span></span>
+        <!--end:Menu link-->
+        <!--begin:Menu sub-->
+
+    </div><!--end:Menu item-->
+    <!--begin:Menu item-->
+
+    <div class="menu-item">
+        <!--begin:Menu link--><a class="menu-link" href="/transacao.php" target=""><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Connector Bank
+            </span></a>
+        <!--end:Menu link-->
+    </div>
+    <!--end:Menu item-->
+
+
+
+
+</div>
+</div>
+<!--end::Menu-->
+
                                         <!--begin::Menu-->
                                         <div class="menu menu-column menu-fit menu-rounded menu-title-gray-600 menu-icon-gray-500 menu-state-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500 fw-semibold fs-5 px-6 my-5 my-lg-0" id="kt_aside_menu" data-kt-menu="true">
 
