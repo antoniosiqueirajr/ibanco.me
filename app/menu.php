e1d5c2f65759a0b4e6acaa9a625fc7470152dfc4
Antonio Siqueira <antoniofsiqueirajr@gmail.com>
2024-02-04 12:11:46 -0300

    Update menu.php

--- app/menu.php
+++ app/menu.php
@@ -1513,14 +1513,14 @@
                                                 </div><!--end:Menu item--><!--begin:Menu item-->
 
                                                 <div class="menu-item">
-                                                    <!--begin:Menu link--><a class="menu-link" href="https://developer.ibanco.me" target="_blank"><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Receber
+                                                    <!--begin:Menu link--><a class="menu-link" href="https://developer.ibanco.me" target="_blank"><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Contato
                                                         </span></a>
                                                     <!--end:Menu link-->
                                                 </div>
                                                 <!--end:Menu item-->
 
                                                 <div class="menu-item">
-                                                    <!--begin:Menu link--><a class="menu-link" href="https://developer.ibanco.me" target="_blank"><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Pagar
+                                                    <!--begin:Menu link--><a class="menu-link" href="https://developer.ibanco.me" target="_blank"><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Financeiro
                                                         </span></a>
                                                     <!--end:Menu link-->
                                                 </div>
@@ -1528,7 +1528,23 @@
 
 
                                                 <div class="menu-item">
-                                                    <!--begin:Menu link--><a class="menu-link" href="https://developer.ibanco.me" target="_blank"><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Extrato
+                                                    <!--begin:Menu link--><a class="menu-link" href="https://developer.ibanco.me" target="_blank"><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Cobranças
+                                                        </span></a>
+                                                    <!--end:Menu link-->
+                                                </div>
+                                                <!--end:Menu item-->
+
+
+                                                <div class="menu-item">
+                                                    <!--begin:Menu link--><a class="menu-link" href="https://developer.ibanco.me" target="_blank"><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Vendas
+                                                        </span></a>
+                                                    <!--end:Menu link-->
+                                                </div>
+                                                <!--end:Menu item-->
+
+
+                                                <div class="menu-item">
+                                                    <!--begin:Menu link--><a class="menu-link" href="https://developer.ibanco.me" target="_blank"><span class="menu-icon"><i class="ki-duotone ki-code fs-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i></span><span class="menu-title">Estoque
                                                         </span></a>
                                                     <!--end:Menu link-->
                                                 </div>
